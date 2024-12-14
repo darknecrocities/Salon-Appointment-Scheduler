30f8a01446ccd34d5ce8085fc977c3ec4dee8561
RhonzKieee <140617917+darknecrocities@users.noreply.github.com>
2024-12-14 14:21:51 +0800

    Create main.php

---
+++ main.php
@@ -0,0 +1,26 @@
+CREATE DATABASE salon;
+
+\c salon
+
+CREATE TABLE customers (
+  customer_id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  phone VARCHAR UNIQUE NOT NULL
+);
+
+CREATE TABLE services (
+  service_id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL
+);
+
+CREATE TABLE appointments (
+  appointment_id SERIAL PRIMARY KEY,
+  customer_id INT REFERENCES customers(customer_id),
+  service_id INT REFERENCES services(service_id),
+  time VARCHAR NOT NULL
+);
+
+INSERT INTO services (name) VALUES 
+('cut'), 
+('color'), 
+('perm');
